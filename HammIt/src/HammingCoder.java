e88d5a3b31c9f056ba4a29f34e2837a39b5b849f
Nicholas Huppert <s3729119@student.rmit.edu.au>
2018-08-04 18:27:28 +1000

    Not really working but close...

--- HammIt/src/HammingCoder.java
+++ HammIt/src/HammingCoder.java
@@ -25,7 +25,7 @@ public class HammingCoder {
 		for( int i = 1; i <= code.length(); i++ ) {
 			tempString += code.charAt(code.length()-i);
 		}
-		
+		System.out.println("Reversed String: " + tempString);
 		code = tempString;
 		tempString = "";
 		
@@ -33,14 +33,38 @@ public class HammingCoder {
 		int lastIndex = 0;
 		for( int i = 0; i < bits; i++ ) {
 			tempIndex = Integer.parseInt(Double.toString(Math.pow(2, i)).substring(0, Double.toString(Math.pow(2, i)).length()-2))-1;
-			tempString += "0" + code.substring(lastIndex, tempIndex < code.length()? tempIndex:code.length());
+			tempString += "0" + code.substring(lastIndex, lastIndex + tempIndex < code.length()? tempIndex:code.length());
 			lastIndex = tempIndex;
 			System.out.println(tempString);
 		}
-		System.out.println(tempString);
+		System.out.println("Reversed string with 0s for parity bits: " + tempString);
+		code = tempString;
 		// Figure out how to mathmatically count parity for each bit and insert said bits
 		
+		boolean parity = true;
+		int currentBit = 0;
 		
+		for( int i = 0; i < bits; i++ ) {
+			// Calc starting position and ammount of bits this parity bit reads
+			tempIndex = Integer.parseInt(Double.toString(Math.pow(2, i)).substring(0, Double.toString(Math.pow(2, i)).length() -2));
+			currentBit = tempIndex;
+			lastIndex = tempIndex - 1;
+			
+			// 2 falses make a true. SO check parity on each substring and toggle our parity only when the result is false
+			// If an even number of parities are found in substrings we will not modify the bit, if an odd number we will
+			while( lastIndex < code.length() ) {
+				if( !checkParity( code.substring( lastIndex, lastIndex + tempIndex < code.length()? lastIndex + tempIndex:code.length())) )
+					parity = !parity;
+				
+				lastIndex += 2 * tempIndex;
+			}
+			
+			if( !parity && currentBit +1 < code.length() )
+				code = code.substring(0, currentBit -1 ) + "1" + code.substring(currentBit , code.length());
+			
+		}
+		
+		System.out.println("Reversed with hamming code: " + code );
 		
 		return code;
 	}
@@ -65,6 +89,7 @@ public class HammingCoder {
 	 * @return True: Even parity, False: Odd parity
 	 */
 	public boolean checkParity( String data ) {
+		System.out.println(data);
 		boolean parity = true;
 		char dataAsChars[] = data.toCharArray();
 		for( char c: dataAsChars ) {
